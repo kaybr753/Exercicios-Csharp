8888237eec151e2f0359dc0877b9576f2616e7da
kaybr753 <7e3f3ef0f43937bc42291a920db024bb@firemail.com.br>
2025-01-05 14:52:17 -0500

    Add files via upload

---
+++ E12/E12/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Elaborar um programa que realize a conversão de R$ para US$, 
+o programa deve saber qual o valor da cotação do dólar e fazer a conversão.
+*/
+decimal taxaCambial, valorDolar, valorReal;
+Console.Title = "Conversão de R$ para US$\n";
+
+Console.WriteLine("Informe  o valor em R$: ");
+valorReal = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Informe a taxa cambial: ");
+taxaCambial = Convert.ToDecimal(Console.ReadLine());
+
+valorDolar = valorReal / taxaCambial;
+Console.WriteLine();
+
+Console.WriteLine($"O valor em US$ é: {valorDolar:F2}");
+
+Console.ReadKey();
+
